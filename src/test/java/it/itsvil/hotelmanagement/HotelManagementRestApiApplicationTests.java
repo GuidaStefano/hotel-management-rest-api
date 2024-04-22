225fdbea117c91f12aabb6095fef2bb967d1cc94
Stefano Guida <stefanoguida.gs@gmail.com>
2024-04-22 10:57:58 +0200

    first commit

---
+++ src/test/java/it/itsvil/hotelmanagement/HotelManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsvil.hotelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
